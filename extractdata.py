8d4c7f5143c457f3e3f50db20160131f746e1121
Nikolay Kalinin <nakalinin@gmail.com>
2016-09-05 18:43:39 +0300

    fixed to work with python3

--- extractdata.py
+++ extractdata.py
@@ -14,28 +14,29 @@ summaryexpr = re.compile(r"<span itemprop=\"summary\">([^<]+)<")
 
 titleexpr = re.compile(r"<h1><[^>]*>([^<]+)<")
 
-data = remscriptsexpr.sub("index.html", data)
+data = open("index.html", "r", encoding="cp1251").read()
+data = remscriptsexpr.sub("", data)
 
 title = titleexpr.search(data).group(1).strip()
-print u"title = {}".format(title)
+print(u"title = {}".format(title))
 
 summary = summaryexpr.search(data).group(1).strip()
-print u"summary = {}".format(summary)
+print(u"summary = {}".format(summary))
 
 timeneeded = timeexpr.search(data)
 if timeneeded is None:
 	timeneeded = ("N/A", "?")
 else:
 	timeneeded = (timeneeded.group(1), timeneeded.group(2).strip())
-print u"time = {}; value = {}".format(timeneeded[0], timeneeded[1])
+print(u"time = {}; value = {}".format(timeneeded[0], timeneeded[1]))
 
 plates = platesexpr.search(data)
 if plates is None:
 	plates = "?"
 else:
 	plates = plates.group(1).strip()
-print u"plates = {}".format(plates)
+print(u"plates = {}".format(plates))
 
 ingredients = ingredientexpr.findall(data)
 for ing in ingredients:
-	print u"id = {}; name = {}; amount = {}".format(ing[0], ing[1], ing[2])
+	print(u"id = {}; name = {}; amount = {}".format(ing[0], ing[1], ing[2]))
