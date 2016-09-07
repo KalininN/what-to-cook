defa2094c04299760591b0bd4fa92d44bc25055e
ShaydurovaEkaterina <ka10a@mail.ru>
2016-09-07 17:56:17 +0300

    Add models IngredientAmount and IngredientName Add new fields in model Recipe

--- whattocook/recipes/models.py
+++ whattocook/recipes/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
 
 
+class IngredientName(models.Model):
+    primary_id = models.IntegerField()
+
+    name = models.CharField(max_length=500)
+
+
+class IngredientAmount(models.Model):
+    primary_id = models.IntegerField()
+
+    amount = models.IntegerField()
+
+    units = models.CharField()
+
+
 class Recipe(models.Model):
-	summary = models.CharField(max_length=500)
-	plates = models.IntegerField(default=1)
+    summary = models.CharField(max_length=500)
+
+    plates = models.IntegerField(default=1)
+
+    time = models.IntegerField(blank=True)
+
+    title = models.CharField(max_length=500)
+
+    ingredients = models.ManyToManyField(IngredientAmount)
